eb460ac4f437cd87f3af331620065ae4a09aadaa
jaimebuiles <jaime.builes@quipux.com>
2024-05-19 07:28:06 -0500

    Created the class auditdatabase of BD.

---
+++ API_Parcial3/Controllers/DAL/Entities/AuditDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Parcial3.Controllers.DAL.Entities
+{
+    public class AuditDatabase
+    {
+        [Key]
+        [Required]
+        public virtual Guid Id { get; set; }
+        public virtual DateTime CreateDate { get; set; }
+        public virtual DateTime ModifiDate { get; set; }
+    }
+}
