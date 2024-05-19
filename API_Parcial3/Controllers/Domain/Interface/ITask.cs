a376f9c9a253501e64b93184d6eff15bd020c80f
jaimebuiles <jaime.builes@quipux.com>
2024-05-19 08:49:25 -0500

    Created the Interface for Task.

---
+++ API_Parcial3/Controllers/Domain/Interface/ITask.cs
@@ -0,0 +1,23 @@
+﻿namespace API_Parcial3.Controllers.Domain.Interface
+{
+    public interface ITask
+    {
+        //Listar tareas ordenadas de la más prioritaria a la menos prioritaria
+        Task<IEnumerable<Task>> GetTaskOrderbyPriorityAsync(Task task);
+
+        //Obtener tarea por ID.
+        Task<Task> GetTaskById(Guid id);
+
+        //Obtener tareas por fecha de vencimiento.
+        Task<IEnumerable<Task>> GetTaskByDueDate(Task task);
+
+        //Crear nuevas tareas.
+        Task<Task> CreateTaskAsync(Task task);
+
+        //Modificar una tarea cuando se complete
+        Task<Task> UpdateTaskCompetedAsync(Task task);
+
+        //Eliminar las tareas que ya fueron completadas.
+        Task<Task> DeleteTaskAsync(Guid id);
+    }
+}
