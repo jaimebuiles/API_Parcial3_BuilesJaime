d96939c73e60a3939c70326b2a28584849326512
jaimebuiles <jaime.builes@quipux.com>
2024-05-19 07:30:43 -0500

    Created the class Task and add the atributes.

---
+++ API_Parcial3/Controllers/DAL/Entities/Task.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Parcial3.Controllers.DAL.Entities
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Titulo")]
+        [MaxLength(50, ErrorMessage = "The field {0} must have a maximum of {1} character")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public String Title { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(100, ErrorMessage = "The campo {0} must have a maximum of {1} character")]
+        public String Description { get; set; }
+
+        [Display(Name = "Completado")]
+        [MaxLength(50, ErrorMessage = "The campo {0} must have a maximum of {1} character")]
+        [Required(ErrorMessage = "Field {0} requires yes or no")]
+        public String IsCompleted { get; set; }
+
+        [Display(Name = "Prioridad")]
+        [MaxLength(50, ErrorMessage = "The field {0} requires the assignment of  (High, Medium, Low)")]
+        [Required]
+        public String Priority { get; set; }
+
+        [Display(Name = "Fecha Vencimiento")]
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Display(Name = "Fecha Creación")]
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Fecha Finalización")]
+        [MaxLength(50, ErrorMessage = "The field {0} requires a date to be assigned")]
+        public DateTime CompletionDate { get; set; }
+    }
+}
