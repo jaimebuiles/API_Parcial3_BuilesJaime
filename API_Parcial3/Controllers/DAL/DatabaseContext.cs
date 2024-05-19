fb941404649f4e32503e7cc2e10642b4a02ba32e
jaimebuiles <jaime.builes@quipux.com>
2024-05-19 07:32:58 -0500

    Add class database Context

---
+++ API_Parcial3/Controllers/DAL/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace API_Parcial3.Controllers.DAL
+{
+    public class DatabaseContext
+    {
+
+
+        #region DbSet
+        
+
+        #endregion
+    }
+}
